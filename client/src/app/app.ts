7c991fab7467f774864c862430250162f3bd9533
LowryZhao <lowryzhao.griffithuni.edu.au>
2025-09-02 10:37:12 +1000

    fix http

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthService } from './services/auth';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
   standalone: true,
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule, HttpClientModule]
 })
 export class AppComponent {
   constructor(public authService: AuthService) {}
